66306ec9e699d547e57659fc7d5399182da84cd9
Andrew0Andrew <software.engineer.andrew@gmail.com>
2023-09-15 23:29:02 +0400

    postgres, scroll, design

--- Frontend/src/pages/Todo.jsx
+++ Frontend/src/pages/Todo.jsx
@@ -1,5 +1,6 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import styles from './Todo.module.css'
+import axios from 'axios'
 const Todo = () => {
     const [title,setTitle]=useState('')
     const [priority,setPriority]=useState('')
@@ -7,18 +8,41 @@ const Todo = () => {
     const handleSubmit = () => {
         if(title !== '' && priority !== '' && priority > 0 ){
             console.log(title,priority)
+            axios.post('http://localhost:3000/tasks',{
+                text:title,
+                priority:priority,
+                
+            }).then((res)=>{
+                console.log(res.data)
+            }).catch((error)=>{
+                console.error(error)
+            })
             setTitle('')
             setPriority('')
-            const task = {
-                title,
-                priority,
+            const object={
+                title:title,
+                priority:priority,
             }
-            setTasks(prev=>[...prev,task])
+            setTasks(prev=>[...prev,object])
         } else {
             console.log('введены не все значения')
         }
-        
     }
+    useEffect(()=>{
+        axios.get('http://localhost:3000/tasks').then((res)=>{
+            setTasks([])
+            for (let index = 0; index < res.data.length; index++) {
+                const element = {
+                    title:res.data[index].text,
+                    priority:res.data[index].priority,
+                };
+                setTasks(prev=>[...prev,element])
+            }
+            console.log(res)
+        }).catch((error)=>{
+            console.error(error)
+        })
+    },[])
   return (
     <div className={styles.note}>
         <h1 className={styles.note_title}>Notes</h1>
@@ -31,17 +55,20 @@ const Todo = () => {
 
 
         </div>
-    {
-        tasks.reverse().map((el,i)=>{
-            return(
-                <>
-                <h1>{el.title}</h1>
-                <h2>{el.priority} Priority</h2>
-                </>
-                
-            )
-        })
-    }
+        <div className={styles.note_list}>
+
+            {
+                tasks.reverse().map((el,i)=>{
+                    return(
+                        <div className={styles.note_list_card}> 
+                            <h1 className={styles.note_list_card_title}>{el.title}</h1>
+                            <h2 className={styles.note_list_card_priority}>{el.priority} Priority</h2>
+                        </div>
+                        
+                    )
+                })
+            }
+        </div> 
 
 
 
